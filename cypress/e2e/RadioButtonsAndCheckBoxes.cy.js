6c5aaf6e92a230eb799b0bfa35381d6a422cff0b
hhakangull <hakanngul@icloud.com>
2023-10-29 16:45:59 +0300

    radiobuttons and checkboxes

--- cypress/e2e/RadioButtonsAndCheckBoxes.cy.js
+++ cypress/e2e/RadioButtonsAndCheckBoxes.cy.js
@@ -1,17 +1,33 @@
 describe("Check UI Elements", () => {
-  let baseURl = "https://demoqa.com";
+  let baseUrl = "https://chercher.tech/practice/popups#";
   Cypress.on("uncaught:exception", (err, runnable) => {
     return false;
   });
-  it("Checking Radio Buttons", () => {
-    cy.visit(baseURl + "/radio-button");
-    //cy.contains("Impressive").should("be.visible").click();
-    //cy.get(".text-success").invoke("text").should("eq", "Impressive");
-    //cy.contains("Yes").should("be.visible").click();
-    //cy.get(".text-success").invoke("text").should("eq", "Yes");
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
+  it.skip("Check Box and Radio Buttons", () => {
+    cy.get("[title=Women]").eq(0).click();
+    cy.get("input#layered_category_4").check().should("be.checked");
+    cy.get("input#layered_category_8").check().should("be.checked");
+    cy.get("#layered_id_attribute_group_1").check().should("be.checked");
+  });
 
-    cy.contains("Impressive").should("be.visible");
-    // Select the radio button by ID
-    cy.get("label[for='yesRadio']").click();
+  it.skip("Radio button tests", () => {
+    cy.get("#radio").check().should("be.checked");
+    cy.get("#checkbox").check().should("be.checked");
+    cy.get("#checkbox").uncheck().should("not.be.checked");
+    cy.get("[name=upload]").then((input) => {
+      const file = new File(["test"], "test.txt", { type: "text/plain" });
+      const fileList = new DataTransfer();
+      fileList.items.add(file);
+      input[0].files = fileList.files;
+      cy.wrap(input).trigger("change", { force: true });
+    });
+  });
+
+  it("checker", () => {
+    cy.get("#checkbox").first().check().should("be.checked");
+    cy.get("#radio").last().check().should("be.checked");
   });
 });
